3fc1f7e1c5d25ea48ba45c95c45e10f6fc391d87
Kumar Alankrit <kr.alankrit@gmail.com>
2023-11-18 18:02:55 +0530

    Merge Sort Algorithm

---
+++ src/MergeSortAlgo.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class MergeSortAlgo {
+    public static void main(String[] args) {
+        int arr[] = {5, 1, 4, 6, 3, 2, 7};
+        MergeSortAlgo.mergeSort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high)
+            return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+
+    }
+
+}
