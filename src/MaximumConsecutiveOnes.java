f8c4b99a1ef4ccc42d345242fa79d4200ca322cd
Kumar Alankrit <kr.alankrit@gmail.com>
2023-11-21 23:42:49 +0530

    Max Consecutive Ones

--- src/MaximumConsecutiveOnes.java
+++ src/MaximumConsecutiveOnes.java
@@ -1,19 +1,19 @@
 public class MaximumConsecutiveOnes {
     public static void main(String[] args) {
-        int a[] = {1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0};
-        System.out.println(maximumOnes(a));
+        int arr[] = {1,1,0,0,1,1,1,1,0,0,1,0,1,1};
+        System.out.println(maximumOnes(arr));
     }
 
-    private static int maximumOnes(int[] a) {
+    private static int maximumOnes(int[] arr) {
         int count = 0;
         int maximum = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == 1) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
                 count++;
+                maximum = Math.max(maximum, count);
             } else {
                 count = 0;
             }
-            maximum = Math.max(maximum, count);
         }
         return maximum;
     }
