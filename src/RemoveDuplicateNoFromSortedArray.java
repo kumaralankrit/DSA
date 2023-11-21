479c0b645477992de61e616c1eec54de4a951c3a
Kumar Alankrit <kr.alankrit@gmail.com>
2023-11-21 18:59:29 +0530

    Remove Dupilcate from Sorted Array

--- src/RemoveDuplicateNoFromSortedArray.java
+++ src/RemoveDuplicateNoFromSortedArray.java
@@ -1,18 +1,18 @@
 public class RemoveDuplicateNoFromSortedArray {
     public static void main(String[] args) {
-        int a[] = {1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 8, 9, 9, 9, 9, 9};
-        int k = removeDuplicate(a);
+        int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 7, 7, 8, 9, 9, 9, 9, 9};
+        int k = removeDuplicate(arr);
         for (int i = 0; i < k; i++) {
-            System.out.print(a[i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 
-    private static int removeDuplicate(int[] a) {
+    private static int removeDuplicate(int[] arr) {
         int i = 0;
-        for (int j = 1; j < a.length; j++) {
-            if (a[i] != a[j]) {
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] != arr[i]) {
+                arr[i+1] = arr[j];
                 i++;
-                a[i] = a[j];
             }
         }
         return i + 1;
