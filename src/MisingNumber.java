487a57afb7fefc64eb6b29419e5066be6ed8b628
Kumar Alankrit <kr.alankrit@gmail.com>
2023-11-21 22:42:26 +0530

    Missing Number

--- src/MisingNumber.java
+++ src/MisingNumber.java
@@ -1,6 +1,6 @@
 public class MisingNumber {
     public static void main(String[] args) {
-        int arr[]={1,2,3,5,4,6};
+        int arr[]={1,2,3,5,6};
         missingNo(arr);
     }
     private static void missingNo(int[] arr) {
