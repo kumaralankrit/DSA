6e83dbf342617a9f54680392ed73323e5dc5ce0d
Kumar Alankrit <kr.alankrit@gmail.com>
2023-11-23 21:14:50 +0530

    Leader Element

--- src/LeadersInArray.java
+++ src/LeadersInArray.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
+
 public class LeadersInArray {
-    //Leader is an element that is greater than all of the elements on its right side in the array.
+    //Leader is an element that is greater than all the elements on its right side in the array.
     public static void main(String[] args) {
-        int a[] = {10, 22, 12, 3, 0, 6};
-        leaderElements(a);
+        int arr[] = {10, 22, 12, 3, 0, 6};
+        System.out.println(leaderElements(arr));
     }
 
-    private static void leaderElements(int[] a) {
-        int max = a[a.length - 1];
-        System.out.println(a[a.length - 1] + " ");
-        for (int i = a.length - 2; i >= 0; i--)
-            if (a[i] > max) {
-                System.out.println(a[i] + " ");
-                max = a[i];
+    private static ArrayList<Integer> leaderElements(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int max = arr[arr.length - 1];
+        ans.add(arr[arr.length - 1]);
+        for (int i = arr.length - 2; i >= 0; i--)
+            if (arr[i] > max) {
+                ans.add(arr[i]);
+                max = arr[i];
             }
+        return ans;
     }
 }
